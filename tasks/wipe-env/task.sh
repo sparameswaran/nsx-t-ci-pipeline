e804dd742fa6170144c52162f3921661a5ee2315
Sabha Parameswaran <sabhap@pivotal.io>
2018-01-05 10:19:48 -0800

    Handle function ref in wipe-env

--- tasks/wipe-env/task.sh
+++ tasks/wipe-env/task.sh
@@ -6,7 +6,7 @@ root=$(pwd)
 export GOVC_TLS_CA_CERTS=/tmp/vcenter-ca.pem
 echo "$GOVC_CA_CERT" > $GOVC_TLS_CA_CERTS
 
-source "${root}/pcf-pipelines/functions/check_opsman_available.sh"
+source "${root}/nsx-t-ci-pipeline/functions/check_opsman_available.sh"
 
 opsman_available=$(check_opsman_available $OPSMAN_DOMAIN_OR_IP_ADDRESS)
 if [[ $opsman_available == "available" ]]; then
