5c69bc5e68241a0ab2424e49cdfd761769b70909
Sabha Parameswaran <sabhap@pivotal.io>
2018-01-09 10:26:18 -0800

    Handle stemcell versions

--- tasks/upload-s3-tile/task.sh
+++ tasks/upload-s3-tile/task.sh
@@ -8,7 +8,8 @@ fi
 FILE_PATH=`find ./s3-tile -name *.pivotal | sort | head -1`
 
 tile_metadata=$(unzip -l $FILE_PATH | grep "metadata" | grep "ml$" | awk '{print $NF}')
-STEMCELL_VERSION_FROM_TILE=$(unzip -p $FILE_PATH $tile_metadata | grep -A4 stemcell | grep version: | awk '{print $NF}' | sed "s/'//g" )
+STEMCELL_VERSION_FROM_TILE=$(unzip -p $FILE_PATH $tile_metadata | grep -A4 stemcell | grep version: \
+                                               | grep -Ei "[0-9]+" | awk '{print $NF}' | sed "s/'//g" )
 
 
 source nsx-t-ci-pipeline/functions/upload_stemcell.sh
