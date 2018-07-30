647fca6fc476faa9d7280634f96b005bdfb88fc0
Sabha Parameswaran <sabhap@pivotal.io>
2018-07-30 13:26:42 -0700

    Handle quotes around stemcell version

--- tasks/upload-s3-tile/task.sh
+++ tasks/upload-s3-tile/task.sh
@@ -12,8 +12,8 @@ source $ROOT_DIR/nsx-t-ci-pipeline/functions/check_null_variables.sh
 TILE_FILE_PATH=`find ./s3-tile -name *.pivotal | sort | head -1`
 
 tile_metadata=$(unzip -l $TILE_FILE_PATH | grep "metadata" | grep "ml$" | awk '{print $NF}')
-stemcell_version_reqd=$(unzip -p $TILE_FILE_PATH $tile_metadata | grep -A4 stemcell | grep version: \
-                                               | grep -Ei "[0-9]{4,}" | awk '{print $NF}' | sed "s/'//g" )
+stemcell_version_reqd=$(unzip -p $TILE_FILE_PATH $tile_metadata | grep -A4 "stemcell_criteria:" | grep "version:" \
+                                               | grep -Ei "[0-9]{2,}" | awk '{print $NF}' | sed "s/'//g;s/\"//g" )
 
 
 if [ -n "$stemcell_version_reqd" ]; then
@@ -41,13 +41,14 @@ set +e
    pivnet-cli download-product-files -p "stemcells" -r $stemcell_version_reqd -g "*${IAAS}*" --accept-eula
    if [ $? != 0 ]; then
      min_version=$(echo $stemcell_version_reqd | awk -F '.' '{print $2}')
+     major_version=$(echo $stemcell_version_reqd | awk -F '.' '{print $1}')
      if [ "$min_version" == "" ]; then
        for min_version in $(seq 0  100)
        do
-          pivnet-cli download-product-files -p "stemcells" -r $stemcell_version_reqd.$min_version -g "*${IAAS}*" --accept-eula && break
+          pivnet-cli download-product-files -p "stemcells" -r $major_version.$min_version -g "*${IAAS}*" --accept-eula && break
        done
      else
-       echo "Stemcell version $stemcell_version_reqd not found !!, giving up"
+       echo "No Stemcell for version $major_version for ${IAAS} found !!, giving up"
        exit 1
      fi
    fi
