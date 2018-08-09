54f72031845a15756de51afb775218c37b030aae
Sabha Parameswaran <sabhap@pivotal.io>
2018-08-09 08:46:09 -0700

    Handle tiles with no stemcells

--- tasks/upload-product-and-stemcell/task.sh
+++ tasks/upload-product-and-stemcell/task.sh
@@ -1,5 +1,25 @@
 #!/bin/bash -eu
 
+# Should the slug contain more than one product, pick only the first.
+FILE_PATH=`find ./pivnet-product -name *.pivotal | sort | head -1`
+om-linux -t https://$OPSMAN_DOMAIN_OR_IP_ADDRESS \
+         -u $OPSMAN_USERNAME \
+         -p $OPSMAN_PASSWORD \
+         -k --request-timeout 3600 \
+         upload-product -p $FILE_PATH
+
+
+SC_FILE_PATH=$(find . -name "bosh*.tgz" | sort | head -1 || true)
+if [ "$SC_FILE_PATH" != "" ]; then
+  echo "Uploading cached stemcell: $SC_FILE_PATH to Ops Mgr"
+  om-linux -t https://$OPSMAN_DOMAIN_OR_IP_ADDRESS \
+          -u $OPSMAN_USERNAME \
+          -p $OPSMAN_PASSWORD \
+          -k --request-timeout 3600 \
+          upload-stemcell -s $SC_FILE_PATH
+  exit 0
+fi
+
 STEMCELL_VERSION_FROM_PRODUCT_METADATA=""
 if [ -e "./pivnet-product/metadata.json" ]; then
   STEMCELL_VERSION_FROM_PRODUCT_METADATA=$(
@@ -26,24 +46,11 @@ STEMCELL_VERSION_FROM_TILE=$(unzip -p pivnet-product/*.pivotal $tile_metadata | 
 STEMCELL_OS_FROM_TILE=$(unzip -p pivnet-product/*.pivotal $tile_metadata | grep -A5 "stemcell_criteria:"  \
                                   | grep "os:" | awk '{print $NF}' | sed "s/'//g;s/\"//g" )
 
-SC_FILE_PATH=$(find . -name "bosh*.tgz" | sort | head -1 || true)
-if [ "$SC_FILE_PATH" != "" ]; then
-  echo "Uploading cached stemcell: $SC_FILE_PATH to Ops Mgr"
-  om-linux -t https://$OPSMAN_DOMAIN_OR_IP_ADDRESS \
-           -u $OPSMAN_USERNAME \
-           -p $OPSMAN_PASSWORD \
-           -k --request-timeout 3600 \
-           upload-stemcell -s $SC_FILE_PATH
-else
-  source nsx-t-ci-pipeline/functions/upload_stemcell.sh
-  echo "No cached stemcell; Will download and then upload stemcell: $SC_FILE_PATH to Ops Mgr"
-  upload_stemcells $STEMCELL_OS_FROM_TILE "$STEMCELL_VERSION_FROM_TILE $STEMCELL_VERSION_FROM_PRODUCT_METADATA"
+if [ "$STEMCELL_OS_FROM_TILE" == "" -o "$STEMCELL_VERSION_FROM_TILE" == "" ]; then
+  echo "No stemcell dependency declared or no version specified in tile, skipping stemcell upload!"
+  exit 0
 fi
 
-# Should the slug contain more than one product, pick only the first.
-FILE_PATH=`find ./pivnet-product -name *.pivotal | sort | head -1`
-om-linux -t https://$OPSMAN_DOMAIN_OR_IP_ADDRESS \
-         -u $OPSMAN_USERNAME \
-         -p $OPSMAN_PASSWORD \
-         -k --request-timeout 3600 \
-         upload-product -p $FILE_PATH
+source nsx-t-ci-pipeline/functions/upload_stemcell.sh
+echo "No cached stemcell; Will download and then upload stemcell: $SC_FILE_PATH to Ops Mgr"
+upload_stemcells "$STEMCELL_OS_FROM_TILE" "$STEMCELL_VERSION_FROM_TILE $STEMCELL_VERSION_FROM_PRODUCT_METADATA"
