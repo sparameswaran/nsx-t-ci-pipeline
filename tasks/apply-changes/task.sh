73e4d7a965998cb22bf658d5083fecf7371886e5
Aaron Gershman <aegershman@gmail.com>
2018-09-05 15:11:20 -0500

    ability to take task

--- tasks/apply-changes/task.sh
+++ tasks/apply-changes/task.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 set -eu
-
+chmod +x om/om-linux
+export PATH:$PATH:/om
 echo "Apply changes"
 
 om-linux \
-  --target "https://${OPSMAN_DOMAIN_OR_IP_ADDRESS}" \
-  --skip-ssl-validation \
-  --username "${OPSMAN_USERNAME}" \
-  --password "${OPSMAN_PASSWORD}" \
-  apply-changes \
-  --ignore-warnings
+	--target "https://${OPSMAN_DOMAIN_OR_IP_ADDRESS}" \
+	--skip-ssl-validation \
+	--username "${OPSMAN_USERNAME}" \
+	--password "${OPSMAN_PASSWORD}" \
+	apply-changes \
+	--ignore-warnings
